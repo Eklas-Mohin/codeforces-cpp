023179e31f09289b207ddc18c9b5923a7ec873f9
Eklas-Mohin <mohin030299@gmail.com>
2024-10-29 23:43:38 +0600

    F - Digits Summation

---
+++ Assiut-University-Training-Newcomers/f_digits_summation.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstring>
+ 
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+ 
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+    
+    string a, b;
+    cin >> a >> b;
+
+    int x = a[a.length() - 1] - 48;
+    int y = b[b.length() - 1] - '0';
+
+    cout << x + y << endl;
+    
+    return 0;
+}
