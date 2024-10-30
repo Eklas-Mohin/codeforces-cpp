7f19226f421054798522025cb17b9447838be2d3
Eklas-Mohin <mohin030299@gmail.com>
2024-10-30 11:48:48 +0600

    W - Mathematical Expression

---
+++ Assiut-University-Training-Newcomers/w_mathematical_expression.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cmath>
+ 
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+ 
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+    
+    int x, y, z, res;
+    char sign, t;
+
+    scanf("%d %c %d %c %d", &x, &sign, &y, &t, &z);
+
+    if (sign == '+') {
+        res = x + y;
+    } else if (sign == '-') {
+        res = x - y;
+    } else {
+        res = x * y;
+    }
+
+    if (z == res) {
+        cout << "Yes" << endl;
+    } else {
+        cout << res << endl;
+    }
+    
+    return 0;
+}
