f4c03d6d7c398f04c3006755d3b626af2cb20b51
Eklas-Mohin <mohin030299@gmail.com>
2024-10-30 06:13:04 +0600

    P - First digit

---
+++ Assiut-University-Training-Newcomers/p_first_digit.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+ 
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+ 
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+    
+    string str;
+    cin >> str;
+    int k = str[0] - 48;
+
+    if (k % 2 == 0) {
+        cout << "EVEN" << endl;
+    } else {
+        cout << "ODD" << endl;
+    }
+    
+    return 0;
+}
