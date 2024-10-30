888d867b98f929f9001826de11931f84ba52dcd9
Eklas-Mohin <mohin030299@gmail.com>
2024-10-30 11:41:55 +0600

    V - Comparison

---
+++ Assiut-University-Training-Newcomers/v_comparision.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <cmath>
+ 
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+ 
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+    
+    int x, y;
+    char sign;
+
+    scanf("%d %c %d", &x, &sign, &y);
+
+    if (sign == '>') {
+        if (x > y) {
+            cout << "Right" << endl;
+        } else {
+            cout << "Wrong" << endl;
+        }
+    } else if (sign == '<') {
+        if (x < y) {
+            cout << "Right" << endl;
+        } else {
+            cout << "Wrong" << endl;
+        }
+    } else {
+        if (x == y) {
+            cout << "Right" << endl;
+        } else {
+            cout << "Wrong" << endl;
+        }
+    }
+    
+    return 0;
+}
