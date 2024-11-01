6dee5694637d6b5a3419f6910288369bb4a119a7
Eklas-Mohin <mohin030299@gmail.com>
2024-11-01 19:51:30 +0600

    C - Searching

---
+++ Assiut-University-Training-Newcomers/b_searching.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    int n, ans = -1, k, arr[100005];
+    
+    cin >> n;
+    for (int i = 0; i < n; ++i) {
+        cin >> arr[i];
+    }
+    cin >> k;
+
+    for (int i = 0; i < n; ++i) {
+        if (arr[i] == k) {
+            ans = i;
+            break;
+        }
+    }
+
+    cout << ans << endl;
+
+    return 0;
+}
