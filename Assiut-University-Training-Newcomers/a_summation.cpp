9f9541c3f2e27d96ccd7cee58729ea1f8d241f7f
Eklas-Mohin <mohin030299@gmail.com>
2024-11-01 19:45:47 +0600

    A - Summation

---
+++ Assiut-University-Training-Newcomers/a_summation.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    long long n, in, sum = 0;
+    cin >> n;
+
+    for (int i = 0; i < n; ++i) {
+        cin >> in;
+        sum += in;
+    }
+
+    cout << abs(sum) << endl;
+
+    return 0;
+}
