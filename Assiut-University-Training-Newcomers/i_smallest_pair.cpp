15dba5b844b105d2e87031889fb33de617c70571
Eklas-Mohin <mohin030299@gmail.com>
2024-11-02 21:15:34 +0600

    I - Smallest Pair

---
+++ Assiut-University-Training-Newcomers/i_smallest_pair.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <algorithm>
+#include <climits>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+ 
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+    
+    int tc;
+    cin >> tc;
+
+    while (tc--) {
+        int n, arr[100];
+        cin >> n;
+        for (int i = 0; i < n; ++i) {
+            cin >> arr[i];
+        }
+        
+        int ans = INT_MAX;
+        for (int i = 0; i < n; ++i) {
+            for (int j = i + 1; j < n; ++j) {
+                int k = arr[i] + arr[j] + j - i;
+                ans = min(ans, k);
+            }
+        }
+
+        cout << ans << endl;
+    }
+
+    return 0;
+}
