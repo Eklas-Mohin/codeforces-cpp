e4d185be142aa7411334d1df91af7eb4359be868
Eklas-Mohin <mohin030299@gmail.com>
2024-11-07 14:35:15 +0600

    W - Mirror Array

---
+++ Assiut-University-Training-Newcomers/w_mirror_array.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    int n, m;
+    cin >> n >> m;
+    int arr[n][m];
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            cin >> arr[i][j];
+        }
+    }
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = m - 1; j >= 0; --j) {
+            cout << arr[i][j] << ' ';
+        }
+        cout << endl;
+    }
+
+
+    return 0;
+}
